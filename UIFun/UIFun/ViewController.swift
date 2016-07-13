ea3c215afc069b3ad340ad1ec5ff434589e94e49
Uskinn <s.nevzor@yahoo.com>
2016-07-13 09:29:08 -0400

    Done

--- UIFun/UIFun/ViewController.swift
+++ UIFun/UIFun/ViewController.swift
@@ -10,18 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     // TODO: Set up IB outlets
-
+    
+    @IBOutlet weak var bucketOfPaintLabel: UIView!
+    @IBOutlet weak var colorSelectedTop: UISegmentedControl!
+    @IBOutlet weak var colorSelectedBottom: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: Set the initial paint color to "red"
+        bucketOfPaintLabel.backgroundColor = UIColor.redColor()
     }
-
+    
     func mixColors(withFirst first: String, second: String) -> String {
         // TODO: Mix colors and return a string indicating the name of the mixed color
         // (e.g., "red", "purple", "blue")
+        
+        switch (first.lowercaseString, second.lowercaseString) {
+        case ("red", "yellow"), ("yellow", "red"):
+            return "orange"
+        case ("red", "blue"), ("blue", "red"):
+            return "purple"
+        case ("yellow", "blue"), ("blue", "yellow"):
+            return "green"
+        case ("yellow", "yellow"):
+            return "yellow"
+        case ("blue", "blue"):
+            return "blue"
+        default:
+            return "red"
+        }
     }
-
+    
     @IBAction func colorSelected(sender: UISegmentedControl) {
         // TODO: Mix each selected color and set the paint color to the mixed color
+        var topColorName = colorSelectedTop.color.name
+        var bottomColorName = colorSelectedBottom.color.name
+        if sender == colorSelectedTop {
+            topColorName = sender.color.name
+        }
+        else {
+            bottomColorName = sender.color.name
+        }
+        bucketOfPaintLabel.paintColorName = mixColors(withFirst: topColorName, second: bottomColorName)
     }
+    
 }
+
+
